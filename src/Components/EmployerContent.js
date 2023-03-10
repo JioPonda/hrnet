65f297743ce16adc29c05a850614dab3625045c7
Jioponda <crazyponda@outlook.fr>
2023-03-10 15:23:08 +0100

    ajout du Navlink vers Home

--- src/Components/EmployerContent.js
+++ src/Components/EmployerContent.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function EmployerContent() {
   return (
     <div id="employee-div" class="container">
       <h1>Current Employees</h1>
       <table id="employee-table" class="display"></table>
-      <a href="index.html">Home</a>
+      <NavLink to="/Home">Home</NavLink>
     </div>
   );
 }
